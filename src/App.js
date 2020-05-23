7da5edcc0ed2c01b173f12855beef11b2ad344d9
Lambda_School_Loaner_282 <lambda_school_loaner_282@Lambda-School-Loaner-282s-MacBook-Pro.local>
2020-05-23 16:44:36 -0500

    First push.  Attempted

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import PostsPage from "./components/PostsContainer/PostsPage";
+import SearchBar from "./components/SearchBar/SearchBarContainer";
+import  dummyData  from "./dummy-data";
 // import the PostsPage and SearchBar and add them to the App
 
 
 const App = () => {
-
+  const [ data, setData ] = useState(dummyData)
+  console.log(dummyData)
   return (
     <div className="App">
+      <PostsPage data={data}/>
+      <SearchBar />
       {/* Add imported components here to render them */}
     </div>
   );
--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,13 +6,17 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
-
+  const [ data, setData ] = useState(Comment)
+ 
+ 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {/* <div>{props.data.map((str, index) => {
+          return {str}})}
+          </div> */}
       <CommentInput />
     </div>
   );
-};
-
+        }
 export default CommentSection;
